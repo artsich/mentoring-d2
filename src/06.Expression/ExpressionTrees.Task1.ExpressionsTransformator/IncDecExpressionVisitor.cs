6d3df84de3f5d864f87c5361e9ae2dbc1036381b
Artsiom_Dubinevich <artsiom_dubinevich@epam.com>
2022-07-21 15:30:17 +0200

    Init project and add Inc/Dec task.

---
+++ src/06.Expression/ExpressionTrees.Task1.ExpressionsTransformator/IncDecExpressionVisitor.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq.Expressions;
+
+namespace ExpressionTrees.Task1.ExpressionsTransformer;
+
+public class IncDecExpressionVisitor : ExpressionVisitor
+{
+    protected override Expression VisitBinary(BinaryExpression node)
+    {
+        if (node.Left.NodeType != ExpressionType.Parameter ||
+            node.Right is not ConstantExpression constant ||
+            constant.Value?.ToString() != "1")
+        {
+            return base.VisitBinary(node);
+        }
+
+        return node.NodeType switch
+        {
+            ExpressionType.Add => Expression.Increment(node.Left),
+            ExpressionType.Subtract => Expression.Decrement(node.Left),
+            _ => base.VisitBinary(node)
+        };
+    }
+}
