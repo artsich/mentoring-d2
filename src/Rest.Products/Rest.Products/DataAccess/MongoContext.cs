99c36569c4ec364c44913c2509b48e3405ae2a3c
JillyMan <artyom543211@gmail.com>
2022-07-30 18:02:01 +0200

    Run app in docker

--- src/Rest.Products/Rest.Products/DataAccess/MongoContext.cs
+++ src/Rest.Products/Rest.Products/DataAccess/MongoContext.cs
@@ -8,18 +8,14 @@ public class DatabaseSetting
     public string ConnectionString { get; set; } = "";
 
     public string Name { get; set; } = "";
-
-    public string[] Collections { get; set; } = Array.Empty<string>();
 }
 
 public class MongoContext
 {
-    private readonly IOptions<DatabaseSetting> _settings;
     private readonly IMongoDatabase _database;
 
     public MongoContext(IOptions<DatabaseSetting> settings)
     {
-        _settings = settings;
         var mongoSettings = MongoClientSettings.FromUrl(
             new MongoUrl(settings.Value.ConnectionString ?? throw new ArgumentException(nameof(settings.Value.ConnectionString)))
         );
