6b8077a3cbfb045f5459bc9157886cf7a0b87615
JillyMan <artyom543211@gmail.com>
2022-08-04 20:12:36 +0200

    Add facade

--- src/Patterns.CuttingShape/Facade/Program.cs
+++ src/Patterns.CuttingShape/Facade/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿new Facade().PlaceOrder("Id1", 20, "my@gmail.com");
+
+class Facade
+{
+	private readonly ProductCatalog productCatalog = new();
+	private readonly PaymentSystem paymentSystem = new();
+	private readonly InvoiceSystem invoiceSystem = new();
+
+	public void PlaceOrder(string productId, int quantity, string email)
+	{
+		var product = productCatalog.GetProductDetails(productId);
+		var cost = quantity * product.Price;
+
+		if (!paymentSystem.MakePayment(new Payment(productId, cost)))
+		{
+			throw new InvalidOperationException("Oops, payment error");
+		}
+
+		invoiceSystem.SendInvoice(new Invoice(product.Name, quantity, cost, email));
+	}
+}
+
+public record ProductDetails(string Id, string Name, double Price);
+public record Payment(string ProductId, double Cost);
+public record Invoice(string ProductName, int Quantity, double Cost, string Email);
+
+class ProductCatalog
+{
+	public ProductDetails GetProductDetails(string productId)
+	{
+		return new ProductDetails("Id1", "Milk", 100);
+	}
+}
+
+class PaymentSystem
+{
+    public bool MakePayment(Payment payment)
+	{
+		return true;
+	}
+}
+
+class InvoiceSystem
+{
+    public void SendInvoice(Invoice invoice)
+	{
+		Console.WriteLine($@"
+Email: {invoice.Email}
+ProductName: {invoice.ProductName}
+Quantity: {invoice.Quantity}
+Cost: {invoice.Cost}
+");
+
+	}
+}
