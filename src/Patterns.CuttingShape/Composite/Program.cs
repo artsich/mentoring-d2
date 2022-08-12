36826c134a2689d3affe1c1e5f12576919c38968
Artsiom Dubinevich <34747326+JillyMan@users.noreply.github.com>
2022-08-12 21:42:23 +0200

    Adapter, Composite and Façade patterns (#14)
    * Add adapter task.
    
    * Add composite exercise.
    
    * Add facade
    
    * fix comnent

---
+++ src/Patterns.CuttingShape/Composite/Program.cs
@@ -0,0 +1,79 @@
+﻿public interface IXmlElement
+{
+	string ConvertToString();
+}
+
+public class InputText : IXmlElement
+{
+	private readonly string name;
+	private readonly string value;
+
+	public InputText(string name, string value)
+	{
+		this.name = name;
+		this.value = value;
+	}
+
+	public string ConvertToString()
+	{
+		return $"<inputText name='{name}' value='{value}'/> ";
+	}
+}
+
+public class LabelText : IXmlElement
+{
+	private readonly string value;
+
+	public LabelText(string value)
+	{
+		this.value = value;
+	}
+
+	public string ConvertToString()
+	{
+		return $"<label value='{value}'/>";
+	}
+}
+
+public class Form : IXmlElement
+{
+	private readonly string name;
+	private readonly List<IXmlElement> elements = new();
+
+	public Form(string name)
+
+	{
+		this.name = name;
+	}
+
+	public Form AddComponent(IXmlElement element)
+	{
+		elements.Add(element ?? throw new ArgumentNullException(nameof(element)));
+		return this;
+	}
+
+	public string ConvertToString()
+	{
+		var body = "\t" + string.Join("\n\t", elements.Select(x => x.ConvertToString()));
+
+		return $@"
+<form name='{name}'>
+{body}
+</form>";
+	}
+}
+
+class Program
+{
+	static void Main()
+	{
+		var xml = new Form("login")
+			.AddComponent(new LabelText("Password"))
+			.AddComponent(new InputText("password", "12345"))
+			.AddComponent(new LabelText("Login"))
+			.AddComponent(new InputText("login", "qwerty"))
+			.ConvertToString();
+
+		Console.WriteLine(xml);
+	}
+}
---
+++ src/Patterns.CuttingShape/Facade/Program.cs
@@ -0,0 +1,75 @@
+﻿new OrderSysem(
+	new ProductCatalog(),
+	new PaymentSystem(),
+	new InvoiceSystem())
+.PlaceOrder("Id1", 20, "my@gmail.com");
+
+class OrderSysem
+{
+	private readonly IProductCatalog productCatalog;
+	private readonly IPaymentSystem paymentSystem;
+	private readonly IInvoiceSystem invoiceSystem;
+
+	public OrderSysem(IProductCatalog productCatalog, IPaymentSystem paymentSystem, IInvoiceSystem invoiceSystem)
+	{
+		this.productCatalog = productCatalog;
+		this.paymentSystem = paymentSystem;
+		this.invoiceSystem = invoiceSystem;
+	}
+
+	public void PlaceOrder(string productId, int quantity, string email)
+	{
+		var product = productCatalog.GetProductDetails(productId);
+		var cost = quantity * product.Price;
+
+		if (!paymentSystem.MakePayment(new Payment(productId, cost)))
+		{
+			throw new InvalidOperationException("Oops, payment error");
+		}
+
+		invoiceSystem.SendInvoice(new Invoice(product.Name, quantity, cost, email));
+	}
+}
+
+public record ProductDetails(string Id, string Name, double Price);
+public record Payment(string ProductId, double Cost);
+public record Invoice(string ProductName, int Quantity, double Cost, string Email);
+
+interface IProductCatalog
+{
+	ProductDetails GetProductDetails(string productId);
+}
+
+interface IPaymentSystem
+{
+	bool MakePayment(Payment payment);
+}
+
+interface IInvoiceSystem
+{
+	void SendInvoice(Invoice invoice);
+}
+
+public class ProductCatalog : IProductCatalog
+{
+	public ProductDetails GetProductDetails(string productId) =>
+		new("Id1", "Milk", 100);
+}
+
+public class PaymentSystem : IPaymentSystem
+{
+	public bool MakePayment(Payment payment) => true;
+}
+
+public class InvoiceSystem : IInvoiceSystem
+{
+	public void SendInvoice(Invoice invoice)
+	{
+		Console.WriteLine($@"
+Email: {invoice.Email}
+ProductName: {invoice.ProductName}
+Quantity: {invoice.Quantity}
+Cost: {invoice.Cost}
+");
+	}
+}
